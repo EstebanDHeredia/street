0c7f84acda25bf2ef8c5734e8231c1ac9c404294
Esteban Heredia <estebanheredia@msn.com>
2023-03-31 16:40:12 -0300

    Se modifica pagina de turnero para que funcione con AJAX, tambien se añaden funcionalidades

--- turnero/urls.py
+++ turnero/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path
 from turnero import views
+from turnero.views import seleccionar_empleado 
+from turnero.views import turnero_empleado_ajax
 
 urlpatterns = [
-    # path('turnos/', lista_turnos, name='lista_turnos'),
-    # path('turnos/nuevo/', TurnoCreateView.as_view(), name='nuevo_turno'),
-    # path('turnos/<int:pk>/editar/', TurnoUpdateView.as_view(), name='editar_turno'),
-    # path('turnos/<int:pk>/eliminar/', TurnoDeleteView.as_view(), name='eliminar_turno'),    
-    path('turnero/<int:empleado_id>/', views.turnero_empleado, name='turnero_empleado'),
-    path('turnero/', views.turnero, name='turnero'),
-    # path('turnero/<int:empleado_id>/<int:year>/<int:month>/<int:day>/', views.turnos_empleado_fecha, name='turnos_empleado_fecha'),
+    path('turnero/', seleccionar_empleado.as_view(), name="turnero"),
+    path('turnero_empleado_ajax/',turnero_empleado_ajax, name='turnero_empleado_ajax'),
 ]
--- turnero/views.py
+++ turnero/views.py
@@ -4,90 +4,45 @@ from django.urls import reverse_lazy
 from tienda.models import Cliente
 from ordenes.models import Empleado, Servicio
 from .models import Turno
+from django.http import Http404
+from django.views.generic import View
+from django.http import JsonResponse
 
 
+def turnero_empleado_ajax(request, *args, **kwargs):
+    print("INGRESE A LA FUNCION AJAX")
+    if request.method == 'GET':
+        empleado = request.GET.get('empleado_id')
+        turnos = Turno.objects.filter(empleado=empleado)
+        print(turnos)
+        # return render(request, 'turnero/seleccionar_empleado.html', {'turnos': turnos})
 
+    eventos = []
+    for turno in turnos:
+        evento = {
+            'id': turno.id,
+            'title': turno.servicio.nombre,
+            'cliente': turno.cliente.nombre.first_name,
+            'start': turno.fecha.strftime('%Y-%m-%dT') + str(turno.get_horario_inicio_display()),
+            'end': turno.fecha.strftime('%Y-%m-%dT') + str(turno.get_horario_fin_display()),
+            'estado': turno.estado,
+            'descripcion': turno.servicio.descripcion,
 
-# Create your views here.
-def turnero_index(request):
-    return render(request, 'turnero/index.html')
+            # 'url': '/turnos/' + str(turno.id),
+        }
+        eventos.append(evento)
 
+    return JsonResponse(eventos, safe=False)
 
-# def lista_turnos(request):
-#     turnos = Turno.objects.all()
-#     return render(request, 'lista_turnos.html', {'turnos': turnos})
+class seleccionar_empleado(View):
+    template = "turnero/seleccionar_empleado.html"
 
-# class TurnoDeleteView(DeleteView):
-#     model = Turno
-#     template_name_suffix = '_confirm_delete'
-#     success_url = reverse_lazy('lista_turnos')
-
-# class TurnoUpdateView(UpdateView):
-#     model = Turno
-#     fields = ['cliente', 'empleado', 'fecha', 'horario', 'servicio', 'estado']
-#     template_name_suffix = '_update_form'
-#     success_url = reverse_lazy('lista_turnos')
-
-
-# def turnos_empleado(request, empleado_id):
-#     empleado = Empleado.objects.get(pk=empleado_id)
-#     turnos = Turno.objects.filter(empleado=empleado)
-#     context = {'turnos': turnos, 'empleado': empleado}
-#     return render(request, 'turnero/turnos_empleado.html', context)
-
-# def turnos_empleado_fecha(request, empleado_id):
-#     empleado = Empleado.objects.get(pk=empleado_id)
-#     fecha_str = request.GET.get('fecha')
-#     fecha = datetime.datetime.strptime(fecha_str, '%Y-%m-%d').date()
-#     turnos = Turno.objects.filter(empleado=empleado, fecha=fecha)
-#     context = {'turnos': turnos, 'empleado': empleado, 'fecha': fecha}
-#     return render(request, 'turnero/turnos_empleado_fecha.html', context)
-
-
-# from django.http import JsonResponse
-# from django.shortcuts import render
-# from .models import Turno
-
-# def turnero_empleado(request, empleado_id):
-#     empleado = Empleado.objects.get(id=empleado_id)
-#     turnos = Turno.objects.filter(empleado=empleado)
-
-#     eventos = []
-#     for turno in turnos:
-#         evento = {
-#             'title': turno.servicio.nombre,
-#             'start': turno.fecha.strftime('%Y-%m-%dT') + turno.horario[1:6],
-#             'end': turno.fecha.strftime('%Y-%m-%dT') + turno.horario[9:14],
-#             'url': '/turnos/' + str(turno.id),
-#         }
-#         eventos.append(evento)
-
-#     return JsonResponse(eventos, safe=False)
-
-def turnero_empleado(request, empleado_id):
-    # Obtendo la lista de empleados que tienen un turno asignado
-    empleados = Empleado.objects.filter(turno__isnull=False).distinct()
-
-    # Obtener los turnos del empleado pasado por parametro
-    turnos = Turno.objects.filter(empleado__id=empleado_id)
-
-    # Pasar los turnos al contexto para su uso en el template
-    context = {
-        'turnos': turnos,
-        'empleados': empleados,
-    }
-    return render(request, 'turnero/turnero_empleado.html', context)
-
-def turnero(request):
-    # Obtener los turnos del empleado
-    empleados = Empleado.objects.all()
-    # print(empleados)
-    turnos = Turno.objects.all()
-
-    # Pasar los turnos al contexto para su uso en el template
-    context = {
-        'turnos': turnos,
-        'empleados': empleados,
-    }
-    print(context)
-    return render(request, 'turnero/turnero_empleado.html', context)
+    def get(self, request):
+        params = {}
+        try:
+            empleados = Empleado.objects.all()
+        except Empleado.DoesNotExist:
+            raise Http404
+        params["empleados"] = empleados
+        
+        return render(request, self.template, params)
